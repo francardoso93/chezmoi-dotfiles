155b81e452549ff044475b5e728d2890d81061e1
Francisco Cardoso <francardoso@outlook.com>
2026-02-06 14:03:22 -0400

    fix: nvim terminal

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Triple-ESC to exit terminal mode (works for all terminals, not just snacks)
+vim.keymap.set("t", "<Esc><Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
