6643ad722221021fd004787c7b82238f8c12a881
Francisco Cardoso <francardoso@outlook.com>
2026-02-15 11:40:03 -0400

    debug keymaps

---
+++ dot_config/nvim/lua/plugins/dap-keys.lua
@@ -0,0 +1,10 @@
+return {
+  "mfussenegger/nvim-dap",
+  keys = {
+    { "<F5>", function() require("dap").continue() end, desc = "Continue" },
+    { "<F10>", function() require("dap").step_over() end, desc = "Step Over" },
+    { "<F11>", function() require("dap").step_into() end, desc = "Step Into" },
+    { "<S-F11>", function() require("dap").step_out() end, desc = "Step Out" },
+    { "<F9>", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+  },
+}
